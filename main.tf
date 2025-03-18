ca7217c5237e0d1147af7c2e6f8f0a2cc3146305
Yaroslav Subota <111CRB111@gmail.com>
2025-03-18 14:07:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,13 @@ variable "dependency_info" {
   default     = null
 }
 
+variable "sensetive_test" {
+  description = "A unique identifier for the resource"
+  type        = string
+  default     = "some sensetive value"
+  sensetive   = true
+}
+
 resource "null_resource" "random_sleep_example_shuf" {
   triggers = {
     force_recreation = timestamp()
